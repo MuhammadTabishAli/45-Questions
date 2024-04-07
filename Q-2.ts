5b5a5f688dbaaaf9baba3a8de76d211ab069d7a1
Muhammad Tabish Ali <162357556+MuhammadTabishAli@users.noreply.github.com>
2024-04-07 12:41:11 +0500

    Update Q-2.ts

--- Q-2.ts
+++ Q-2.ts
@@ -1,4 +1,4 @@
 // Q-2
 
 let personName:string= "Tabish";
-console.log("Hello" ,personName, "would you like to learn some Python today?");
+console.log("Hello" ,personName, "would you like to learn some typescript today?");
